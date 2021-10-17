2db8d0468f8157ad23a6e9da8108112e5b4b9bdf
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-10-18 05:03:47 +0900

    mac compatible VideoView

--- Sources/LiveKit/views/VideoView.swift
+++ Sources/LiveKit/views/VideoView.swift
@@ -3,39 +3,49 @@ import WebRTC
 import CoreMedia
 
 #if !os(macOS)
-
 import UIKit
+public typealias NativeView = UIView
+#else
+// macOS
+public typealias NativeView = NSView
+#endif
 
-public class VideoView: UIView {
+public class VideoView: NativeView {
     var size: CGSize = .zero
-    public private(set) var renderer: RTCVideoRenderer
 
-    required init?(coder decoder: NSCoder) {
+    public private(set) lazy var renderer: RTCVideoRenderer = {
         #if arch(arm64)
+
+        #if !os(macOS)
         let view = RTCMTLVideoView()
         view.videoContentMode = .scaleAspectFill
-        renderer = view
+        view.delegate = self
         #else
-        let view = RTCEAGLVideoView()
-        renderer = view
+        // macOS
+        let view = RTCMTLNSVideoView()
+        view.delegate = self
         #endif
+
+        #else
+        // intel
+        let view = RTCEAGLVideoView()
+        view.delegate = self
+        #endif
+        return view
+    }()
+
+    required init?(coder decoder: NSCoder) {
+//        renderer = createRTCVideoView(delegate: self)
         super.init(coder: decoder)
     }
 
     override public init(frame: CGRect) {
-        #if arch(arm64)
-        let view = RTCMTLVideoView(frame: frame)
-        view.videoContentMode = .scaleAspectFill
-        renderer = view
-        #else
-        let view = RTCEAGLVideoView(frame: frame)
-        renderer = view
-        #endif
 
         super.init(frame: frame)
-        backgroundColor = .black
-        view.delegate = self
-        let rendererView = renderer as! UIView
+
+//        backgroundColor = .black
+//        view.delegate = self
+        let rendererView = self.renderer as! NativeView
         rendererView.translatesAutoresizingMaskIntoConstraints = false
         addSubview(rendererView)
 
@@ -47,22 +57,25 @@ public class VideoView: UIView {
 }
 
 extension VideoView: RTCVideoViewDelegate {
+
     public func videoView(_: RTCVideoRenderer, didChangeVideoSize size: CGSize) {
         // let orientation = UIDevice.current.orientation
         self.size = size
 
-        UIView.animate(withDuration: 0.4) {
+//        UIView.animate(withDuration: 0.4) {
             let defaultAspectRatio = CGSize(width: 4, height: 3)
             let aspectRatio = size == .zero ? defaultAspectRatio : size
             let videoFrame = AVMakeRect(aspectRatio: aspectRatio, insideRect: self.bounds)
 
-            let rendererView = self.renderer as! UIView
+            let rendererView = self.renderer as! NativeView
             rendererView.widthAnchor.constraint(equalToConstant: videoFrame.width).isActive = true
             rendererView.heightAnchor.constraint(equalToConstant: videoFrame.height).isActive = true
 
-            self.layoutIfNeeded()
-        }
+#if !os(macOS)
+        self.layoutIfNeeded()
+#else
+        self.layoutSubtreeIfNeeded()
+#endif
+
     }
 }
-
-#endif
