7c1ae244ffc184e387ced4fb3e12f361f407c7d4
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-10-21 14:51:10 +0900

    videoview

--- Sources/LiveKit/views/VideoView.swift
+++ Sources/LiveKit/views/VideoView.swift
@@ -4,14 +4,13 @@ import CoreMedia
 
 #if !os(macOS)
 import UIKit
-public typealias NativeView = UIView
+public typealias NativeViewType = UIView
 #else
 // macOS
-public typealias NativeView = NSView
+public typealias NativeViewType = NSView
 #endif
 
-public class VideoView: NativeView {
-    var size: CGSize = .zero
+public class VideoView: NativeViewType {
 
     public private(set) lazy var renderer: RTCVideoRenderer = {
         #if arch(arm64)
@@ -41,39 +40,63 @@ public class VideoView: NativeView {
     override public init(frame: CGRect) {
         super.init(frame: frame)
 
-//        backgroundColor = .black
-//        view.delegate = self
-        let rendererView = renderer as! NativeView
-        rendererView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(rendererView)
+#if !os(macOS)
+        backgroundColor = .black
+#endif
 
-        NSLayoutConstraint.activate([
-            rendererView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            rendererView.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
+        //        view.delegate = self
+        let rendererView = renderer as! NativeViewType
+//        rendererView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(rendererView)
+        layoutRenderView()
+
+#if !os(macOS)
+//        rendererView.backgroundColor = .red
+#endif
+
+//        NSLayoutConstraint.activate([
+//            rendererView.centerXAnchor.constraint(equalTo: centerXAnchor),
+//            rendererView.centerYAnchor.constraint(equalTo: centerYAnchor)
+//        ])
     }
+
+    private func layoutRenderView() {
+        (renderer as! NativeViewType).frame = self.bounds
+    }
+
+#if !os(macOS)
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+        layoutRenderView()
+    }
+#else
+    public override func layout() {
+        super.layout()
+        layoutRenderView()
+    }
+#endif
 }
 
 extension VideoView: RTCVideoViewDelegate {
 
     public func videoView(_: RTCVideoRenderer, didChangeVideoSize size: CGSize) {
-        // let orientation = UIDevice.current.orientation
-        self.size = size
-
-//        UIView.animate(withDuration: 0.4) {
-            let defaultAspectRatio = CGSize(width: 4, height: 3)
-            let aspectRatio = size == .zero ? defaultAspectRatio : size
-            let videoFrame = AVMakeRect(aspectRatio: aspectRatio, insideRect: self.bounds)
-
-            let rendererView = self.renderer as! NativeView
-            rendererView.widthAnchor.constraint(equalToConstant: videoFrame.width).isActive = true
-            rendererView.heightAnchor.constraint(equalToConstant: videoFrame.height).isActive = true
-
-#if !os(macOS)
-        self.layoutIfNeeded()
-#else
-        self.layoutSubtreeIfNeeded()
-#endif
-
+//        // let orientation = UIDevice.current.orientation
+//        self.size = size
+//
+////        UIView.animate(withDuration: 0.4) {
+//            let defaultAspectRatio = CGSize(width: 4, height: 3)
+//            let aspectRatio = size == .zero ? defaultAspectRatio : size
+//            let videoFrame = AVMakeRect(aspectRatio: aspectRatio, insideRect: self.bounds)
+//
+//            let rendererView = self.renderer as! NativeView
+//            rendererView.widthAnchor.constraint(equalToConstant: videoFrame.width).isActive = true
+//            rendererView.heightAnchor.constraint(equalToConstant: videoFrame.height).isActive = true
+//
+//#if !os(macOS)
+//        self.layoutIfNeeded()
+//#else
+//        self.layoutSubtreeIfNeeded()
+//#endif
+//
     }
 }
