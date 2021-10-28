235891575b2ff9b7b669dbfa5803cbcad9a15e55
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-10-28 23:57:10 +0900

    add doc for `MulticastDelegate`

--- Sources/LiveKit/support/MulticastDelegate.swift
+++ Sources/LiveKit/support/MulticastDelegate.swift
@@ -1,12 +1,17 @@
 import Foundation
 import Promises
 
-// simplify generic constraints but check type at add/remove
+/// A class that allows to have multiple delegates instead of one.
+///
+/// Uses `NSHashTable` internally to maintain a set of weak delegates.
+///
+/// > Note: `NSHashTable` may not immediately deinit the un-referenced object, due to Apple's implementation.
 public class MulticastDelegate<T>: NSObject {
 
     private let lock = NSLock()
     private let set = NSHashTable<AnyObject>.weakObjects()
 
+    /// Add a single delegate.
     public func add(delegate: T) {
 
         guard let delegate = delegate as AnyObject? else {
@@ -21,7 +26,9 @@ public class MulticastDelegate<T>: NSObject {
         logger.debug("[\(self) MulticastDelegate] count updated: \(self.set.count)")
     }
 
-    // in most cases this is not required to be called since all delegates are weak
+    /// Remove a single delegate.
+    ///
+    /// In most cases this is not required to be called explicitly since all delegates are weak.
     public func remove(delegate: T) {
 
         guard let delegate = delegate as AnyObject? else {
@@ -53,41 +60,4 @@ public class MulticastDelegate<T>: NSObject {
             try fnc(delegate)
         }
     }
-
-    //    internal func wait<V>(timeout: TimeInterval,
-    //                  onTimeout: Error = InternalError.timeout(),
-    //                  builder: (@escaping (V) -> Void) -> T) -> Promise<V> {
-    //
-    //        let promise = Promise<V>.pending()
-    //        var timer: DispatchWorkItem?
-    //        var delegate: T?
-    //
-    //        let completeFnc: (V) -> Void = { value in
-    //            // cancel timer
-    //            timer?.cancel()
-    //            promise.fulfill(value)
-    //            // stop listening
-    //            DispatchQueue.main.async {
-    //                self.remove(delegate: delegate!)
-    //            }
-    //        }
-    //
-    //        let failFnc = {
-    //            promise.reject(onTimeout)
-    //            // stop listening
-    //            DispatchQueue.main.async {
-    //                self.remove(delegate: delegate!)
-    //            }
-    //        }
-    //
-    //        delegate = builder(completeFnc)
-    //        add(delegate: delegate!)
-    //
-    //        // start timer
-    //        logger.debug("[MulticastDelegate] started timer")
-    //        timer = DispatchWorkItem() { failFnc() }
-    //        DispatchQueue.main.asyncAfter(deadline: .now() + timeout, execute: timer!)
-    //
-    //        return promise
-    //    }
 }
