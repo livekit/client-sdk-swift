0fe09b3962e19569f26060a46f0c3cd0305123ef
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-11 22:18:02 +0900

    depend on swift-collections

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.1")),
     ],
     targets: [
         .target(
@@ -28,6 +29,7 @@ let package = Package(
             dependencies: [
                 "WebRTC", "SwiftProtobuf", "Promises",
                 .product(name: "Logging", package: "swift-log"),
+                .product(name: "Collections", package: "swift-collections"),
             ],
             path: "Sources"
         ),
