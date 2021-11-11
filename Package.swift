48e05912b0c3863e31dc8fe85f9a29fb782f96a7
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-11 22:14:11 +0900

    loosen WebRTC ver constraint

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("92.4515.10")),
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.15.0")),
+        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .upToNextMajor(from:"92.4515.11")),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
     ],
