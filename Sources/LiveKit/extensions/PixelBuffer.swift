d8b29d1b8841c10d961de3ecc331367b184cc768
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-14 07:35:35 +0700

    `CVPixelBuffer` conversion extension

---
+++ Sources/LiveKit/extensions/PixelBuffer.swift
@@ -0,0 +1,62 @@
+import Foundation
+import CoreImage
+
+extension CVPixelBuffer {
+    public static func from(_ data: Data, width: Int, height: Int, pixelFormat: OSType) -> CVPixelBuffer {
+        data.withUnsafeBytes { buffer in
+            var pixelBuffer: CVPixelBuffer!
+
+            let result = CVPixelBufferCreate(kCFAllocatorDefault, width, height, pixelFormat, nil, &pixelBuffer)
+            guard result == kCVReturnSuccess else { fatalError() }
+
+            CVPixelBufferLockBaseAddress(pixelBuffer, [])
+            defer { CVPixelBufferUnlockBaseAddress(pixelBuffer, []) }
+
+            var source = buffer.baseAddress!
+
+            for plane in 0 ..< CVPixelBufferGetPlaneCount(pixelBuffer) {
+                let dest      = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, plane)
+                let height      = CVPixelBufferGetHeightOfPlane(pixelBuffer, plane)
+                let bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, plane)
+                let planeSize = height * bytesPerRow
+
+                memcpy(dest, source, planeSize)
+                source += planeSize
+            }
+
+            return pixelBuffer
+        }
+    }
+}
+
+extension Data {
+
+    public init(pixelBuffer: CVPixelBuffer) {
+        CVPixelBufferLockBaseAddress(pixelBuffer, [.readOnly])
+        defer { CVPixelBufferUnlockBaseAddress(pixelBuffer, [.readOnly]) }
+
+        // Calculate sum of planes' size
+        var totalSize = 0
+        for plane in 0 ..< CVPixelBufferGetPlaneCount(pixelBuffer) {
+            let height      = CVPixelBufferGetHeightOfPlane(pixelBuffer, plane)
+            let bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, plane)
+            let planeSize   = height * bytesPerRow
+            totalSize += planeSize
+        }
+
+        guard let rawFrame = malloc(totalSize) else { fatalError() }
+        var dest = rawFrame
+
+        for plane in 0 ..< CVPixelBufferGetPlaneCount(pixelBuffer) {
+            let source      = CVPixelBufferGetBaseAddressOfPlane(pixelBuffer, plane)
+            let height      = CVPixelBufferGetHeightOfPlane(pixelBuffer, plane)
+            let bytesPerRow = CVPixelBufferGetBytesPerRowOfPlane(pixelBuffer, plane)
+            let planeSize   = height * bytesPerRow
+
+            memcpy(dest, source, planeSize)
+            dest += planeSize
+        }
+
+        self.init(bytesNoCopy: rawFrame, count: totalSize, deallocator: .free)
+    }
+}
