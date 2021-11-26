6d0a80c8315662ee173210b73b24f2e96ce63ede
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2021-11-26 16:52:36 +0700

    dont shadow local variable

--- Sources/LiveKit/Core/Transport.swift
+++ Sources/LiveKit/Core/Transport.swift
@@ -32,10 +32,9 @@ internal class Transport: MulticastDelegate<TransportDelegate> {
          delegate: TransportDelegate) throws {
 
         // try create peerConnection
-        let pc = Engine.factory.peerConnection(with: config,
-                                               constraints: RTCMediaConstraints.defaultPCConstraints,
-                                               delegate: nil)
-        guard let pc = pc else {
+        guard let pc = Engine.factory.peerConnection(with: config,
+                                                     constraints: RTCMediaConstraints.defaultPCConstraints,
+                                                     delegate: nil) else {
             throw EngineError.webRTC("failed to create peerConnection")
         }
 
