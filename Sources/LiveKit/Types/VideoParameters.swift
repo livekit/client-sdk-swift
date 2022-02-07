2fa5c35c7b057a02dacb17d8cda07a03633426da
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-02-07 21:58:20 +0700

    clean up VideoParameters

--- Sources/LiveKit/Types/VideoParameters.swift
+++ Sources/LiveKit/Types/VideoParameters.swift
@@ -29,23 +29,40 @@ extension Collection where Element == VideoParameters {
 
 public struct VideoParameters {
 
+    public let dimensions: Dimensions
+    public let encoding: VideoEncoding
+
+    public init(dimensions: Dimensions, encoding: VideoEncoding) {
+        self.dimensions = dimensions
+        self.encoding = encoding
+    }
+}
+
+// MARK: - Presets
+
+public extension VideoParameters {
+
     // 4:3 aspect ratio
     public static let presetQVGA43 = VideoParameters(
         dimensions: Dimensions(width: 240, height: 180),
         encoding: VideoEncoding(maxBitrate: 90_000, maxFps: 10)
     )
+
     public static let presetVGA43 = VideoParameters(
         dimensions: Dimensions(width: 480, height: 360),
         encoding: VideoEncoding(maxBitrate: 225_000, maxFps: 20)
     )
+
     public static let presetQHD43 = VideoParameters(
         dimensions: Dimensions(width: 720, height: 540),
         encoding: VideoEncoding(maxBitrate: 450_000, maxFps: 25)
     )
+
     public static let presetHD43 = VideoParameters(
         dimensions: Dimensions(width: 960, height: 720),
         encoding: VideoEncoding(maxBitrate: 1_500_000, maxFps: 30)
     )
+
     public static let presetFHD43 = VideoParameters(
         dimensions: Dimensions(width: 1440, height: 1080),
         encoding: VideoEncoding(maxBitrate: 2_800_000, maxFps: 30)
@@ -56,18 +73,22 @@ public struct VideoParameters {
         dimensions: Dimensions(width: 320, height: 180),
         encoding: VideoEncoding(maxBitrate: 120_000, maxFps: 10)
     )
+
     public static let presetVGA169 = VideoParameters(
         dimensions: Dimensions(width: 640, height: 360),
         encoding: VideoEncoding(maxBitrate: 300_000, maxFps: 20)
     )
+
     public static let presetQHD169 = VideoParameters(
         dimensions: Dimensions(width: 960, height: 540),
         encoding: VideoEncoding(maxBitrate: 600_000, maxFps: 25)
     )
+
     public static let presetHD169 = VideoParameters(
         dimensions: Dimensions(width: 1280, height: 720),
         encoding: VideoEncoding(maxBitrate: 2_000_000, maxFps: 30)
     )
+
     public static let presetFHD169 = VideoParameters(
         dimensions: Dimensions(width: 1920, height: 1080),
         encoding: VideoEncoding(maxBitrate: 3_000_000, maxFps: 30)
@@ -78,29 +99,41 @@ public struct VideoParameters {
         dimensions: Dimensions(width: 640, height: 360),
         encoding: VideoEncoding(maxBitrate: 200_000, maxFps: 3)
     )
+
     public static let presetScreenShareHD5 = VideoParameters(
         dimensions: Dimensions(width: 1280, height: 720),
         encoding: VideoEncoding(maxBitrate: 400_000, maxFps: 5)
     )
+
     public static let presetScreenShareHD15 = VideoParameters(
         dimensions: Dimensions(width: 1280, height: 720),
         encoding: VideoEncoding(maxBitrate: 1_000_000, maxFps: 15)
     )
+
     public static let presetScreenShareFHD15 = VideoParameters(
         dimensions: Dimensions(width: 1920, height: 1080),
         encoding: VideoEncoding(maxBitrate: 1_500_000, maxFps: 15)
     )
+
     public static let presetScreenShareFHD30 = VideoParameters(
         dimensions: Dimensions(width: 1920, height: 1080),
         encoding: VideoEncoding(maxBitrate: 3_000_000, maxFps: 30)
     )
 
     public static let presets43 = [
-        presetQVGA43, presetVGA43, presetQHD43, presetHD43, presetFHD43
+        presetQVGA43,
+        presetVGA43,
+        presetQHD43,
+        presetHD43,
+        presetFHD43
     ]
 
     public static let presets169 = [
-        presetQVGA169, presetVGA169, presetQHD169, presetHD169, presetFHD169
+        presetQVGA169,
+        presetVGA169,
+        presetQHD169,
+        presetHD169,
+        presetFHD169
     ]
 
     public static let presetsScreenShare = [
@@ -110,68 +143,4 @@ public struct VideoParameters {
         presetScreenShareFHD15,
         presetScreenShareFHD30
     ]
-
-    public let dimensions: Dimensions
-    public let encoding: VideoEncoding
-
-    init(dimensions: Dimensions, encoding: VideoEncoding) {
-        self.dimensions = dimensions
-        self.encoding = encoding
-    }
-
-    //    static func getPresetForDimension(width: Int, height: Int) -> VideoParameters {
-    //        var preset = presets169[0]
-    //        for p in presets169 {
-    //            if width >= p.capture.width, height >= p.capture.height {
-    //                preset = p
-    //            }
-    //        }
-    //        return preset
-    //    }
-
-    //    /// creates encoding parameters that best match input width/height
-    //    static func getRTPEncodingParams(inputWidth: Int, inputHeight: Int, rid: String?, encoding: VideoEncoding? = nil) -> RTCRtpEncodingParameters? {
-    //        var scaleDownFactor = 1.0
-    //        if rid == "h" {
-    //            scaleDownFactor = 2.0
-    //        } else if rid == "q" {
-    //            scaleDownFactor = 4.0
-    //        }
-    //        var targetWidth = Int(Double(inputWidth) / scaleDownFactor)
-    //        var targetHeight = Int(Double(inputHeight) / scaleDownFactor)
-    //
-    //        var selectedEncoding: VideoEncoding
-    //
-    //        if targetWidth < simulcastMinWidth {
-    //            return nil
-    //        }
-    //
-    //        // unless it's original, find the best resolution
-    //        if scaleDownFactor != 1.0 || encoding == nil {
-    //            let preset = getPresetForDimension(width: targetWidth, height: targetHeight)
-    //            targetWidth = preset.capture.width
-    //            scaleDownFactor = Double(inputWidth) / Double(targetWidth)
-    //            targetHeight = Int(Double(inputHeight) / scaleDownFactor)
-    //
-    //            selectedEncoding = preset.encoding
-    //        } else {
-    //            selectedEncoding = encoding!
-    //        }
-    //
-    //        let params = RTCRtpEncodingParameters()
-    //        params.isActive = true
-    //        params.rid = rid
-    //        params.scaleResolutionDownBy = NSNumber(value: scaleDownFactor)
-    //        params.maxFramerate = NSNumber(value: selectedEncoding.maxFps)
-    //        params.maxBitrateBps = NSNumber(value: selectedEncoding.maxBitrate)
-    //        // only set on the full track
-    //        if scaleDownFactor == 1.0 {
-    //            params.networkPriority = .high
-    //            params.bitratePriority = 4.0
-    //        } else {
-    //            params.networkPriority = .low
-    //            params.bitratePriority = 1.0
-    //        }
-    //        return params
-    //    }
 }
