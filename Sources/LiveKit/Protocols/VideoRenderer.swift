84704b13328188fd08608cec3126cea8c85662b0
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-02-16 16:56:29 +0700

    remove unused code

--- Sources/LiveKit/Protocols/VideoRenderer.swift
+++
@@ -1,8 +0,0 @@
-import CoreMedia
-import Foundation
-
-public protocol VideoRenderer {
-    func renderFrame(_ frame: VideoFrame)
-    func updateVideo(size: Dimensions, orientation: VideoOrientation)
-    func invalidateRenderer()
-}
