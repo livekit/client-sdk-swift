6d72edc4728c564941802d9bbf0dadf997532f10
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-02-16 14:41:42 +0700

    cleaner swiftUI view builder code

--- Sources/LiveKit/SwiftUI/SwiftUIVideoView.swift
+++ Sources/LiveKit/SwiftUI/SwiftUIVideoView.swift
@@ -2,11 +2,11 @@ import SwiftUI
 
 #if os(iOS)
 import UIKit
-public typealias NativeViewRepresentable = UIViewRepresentable
+public typealias NativeViewRepresentableType = UIViewRepresentable
 #else
 // macOS
 import AppKit
-public typealias NativeViewRepresentable = NSViewRepresentable
+public typealias NativeViewRepresentableType = NSViewRepresentable
 #endif
 
 /// This class receives delegate events since a struct can't be used for a delegate
@@ -35,6 +35,9 @@ class SwiftUIVideoViewDelegateReceiver: TrackDelegate, Loggable {
 /// A ``VideoView`` that can be used in SwiftUI.
 /// Supports both iOS and macOS.
 public struct SwiftUIVideoView: NativeViewRepresentable {
+
+    typealias ViewType = VideoView
+
     /// Pass a ``VideoTrack`` of a ``Participant``.
     let track: VideoTrack
     let mode: VideoView.Mode
@@ -61,44 +64,61 @@ public struct SwiftUIVideoView: NativeViewRepresentable {
         self.track.add(delegate: delegateReceiver)
     }
 
+    public func makeView(context: Context) -> VideoView {
+        let view = VideoView(preferMetal: preferMetal)
+        updateView(view, context: context)
+        return view
+    }
+
+    public func updateView(_ videoView: VideoView, context: Context) {
+        videoView.track = track
+        videoView.mode = mode
+        videoView.mirrored = mirrored
+        videoView.preferMetal = preferMetal
+    }
+
+    public static func dismantleView(_ videoView: VideoView, coordinator: ()) {
+        videoView.track = nil
+    }
+}
+
+// MARK: - NativeViewRepresentable
+
+// multiplatform version of UI/NSViewRepresentable
+protocol NativeViewRepresentable: NativeViewRepresentableType {
+    /// The type of view to present.
+    associatedtype ViewType: NativeView
+
+    func makeView(context: Self.Context) -> Self.ViewType
+    func updateView(_ nsView: Self.ViewType, context: Self.Context)
+    static func dismantleView(_ nsView: Self.ViewType, coordinator: Self.Coordinator)
+}
+
+extension NativeViewRepresentable {
+
     #if os(iOS)
-    // iOS
-
-    public func makeUIView(context: Context) -> VideoView {
-        let view = VideoView(preferMetal: preferMetal)
-        updateUIView(view, context: context)
-        return view
+    public func makeUIView(context: Context) -> Self.ViewType {
+        return makeView(context: context)
     }
 
-    public func updateUIView(_ videoView: VideoView, context: Context) {
-        videoView.track = track
-        videoView.mode = mode
-        videoView.mirrored = mirrored
-        videoView.preferMetal = preferMetal
+    public func updateUIView(_ view: Self.ViewType, context: Context) {
+        updateView(view, context: context)
     }
 
-    public static func dismantleUIView(_ videoView: VideoView, coordinator: ()) {
-        videoView.track = nil
+    public static func dismantleUIView(_ view: Self.ViewType, coordinator: Self.Coordinator) {
+        dismantleView(view, coordinator: coordinator)
     }
-    #else
-    // macOS
-
-    public func makeNSView(context: Context) -> VideoView {
-        let view = VideoView(preferMetal: preferMetal)
-        updateNSView(view, context: context)
-        return view
+    #elseif os(macOS)
+    public func makeNSView(context: Context) -> Self.ViewType {
+        return makeView(context: context)
     }
 
-    public func updateNSView(_ videoView: VideoView, context: Context) {
-        videoView.track = track
-        videoView.mode = mode
-        videoView.mirrored = mirrored
-        videoView.preferMetal = preferMetal
+    public func updateNSView(_ view: Self.ViewType, context: Context) {
+        updateView(view, context: context)
     }
 
-    public static func dismantleNSView(_ videoView: VideoView, coordinator: ()) {
-        videoView.track = nil
+    public static func dismantleNSView(_ view: Self.ViewType, coordinator: Self.Coordinator) {
+        dismantleView(view, coordinator: coordinator)
     }
-
     #endif
 }
