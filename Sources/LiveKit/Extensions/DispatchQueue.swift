cffad1e7998633a8c9089810a208180c7d57df4e
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-02-21 19:18:08 +0700

    change capture queue priority

--- Sources/LiveKit/Extensions/DispatchQueue.swift
+++ Sources/LiveKit/Extensions/DispatchQueue.swift
@@ -4,7 +4,7 @@ internal extension DispatchQueue {
 
     static let sdk = DispatchQueue(label: "LiveKitSDK", qos: .userInitiated)
     static let webRTC = DispatchQueue(label: "LiveKitSDK.webRTC", qos: .background)
-    static let capture = DispatchQueue(label: "LiveKitSDK.capture", qos: .background)
+    static let capture = DispatchQueue(label: "LiveKitSDK.capture", qos: .userInitiated)
 
     // execute work on the main thread if not already on the main thread
     static func mainSafeSync<T>(execute work: () throws -> T) rethrows -> T {
