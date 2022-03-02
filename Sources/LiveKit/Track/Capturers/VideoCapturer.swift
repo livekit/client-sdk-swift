e6ca74dc8c6d247bede3a1ec9e079d88f9f747d5
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-03-02 12:58:13 +0900

    docs for `BufferCapturer`

--- Sources/LiveKit/Track/Capturers/VideoCapturer.swift
+++ Sources/LiveKit/Track/Capturers/VideoCapturer.swift
@@ -73,7 +73,13 @@ internal extension VideoCapturer {
 public class VideoCapturer: MulticastDelegate<VideoCapturerDelegate>, VideoCapturerProtocol {
 
     /// Array of supported pixel formats that can be used to capture a frame.
-    static let supportedPixelFormats = DispatchQueue.webRTC.sync { RTCCVPixelBuffer.supportedPixelFormats() }
+    ///
+    /// Usually the following formats are supported but it is recommended to confirm at run-time:
+    /// `kCVPixelFormatType_420YpCbCr8BiPlanarVideoRange`,
+    /// `kCVPixelFormatType_420YpCbCr8BiPlanarFullRange`,
+    /// `kCVPixelFormatType_32BGRA`,
+    /// `kCVPixelFormatType_32ARGB`.
+    public static let supportedPixelFormats = DispatchQueue.webRTC.sync { RTCCVPixelBuffer.supportedPixelFormats() }
 
     public enum State {
         case stopped
