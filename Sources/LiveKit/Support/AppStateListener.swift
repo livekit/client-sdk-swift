f4026bdab850eba80f6773776c782eaf1f216556
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-03-06 00:11:55 +0900

    impl

--- Sources/LiveKit/Support/AppStateListener.swift
+++ Sources/LiveKit/Support/AppStateListener.swift
@@ -6,6 +6,7 @@ import UIKit
 internal protocol AppStateDelegate: AnyObject {
     func appDidEnterBackground()
     func appWillEnterForeground()
+    func appWillTerminate()
 }
 
 internal class AppStateListener: MulticastDelegate<AppStateDelegate> {
@@ -22,7 +23,7 @@ internal class AppStateListener: MulticastDelegate<AppStateDelegate> {
                            object: nil,
                            queue: OperationQueue.main) { (_) in
 
-            self.log("didEnterBackground")
+            self.log("UIScene.didEnterBackground")
             self.notify { $0.appDidEnterBackground() }
         }
 
@@ -30,8 +31,16 @@ internal class AppStateListener: MulticastDelegate<AppStateDelegate> {
                            object: nil,
                            queue: OperationQueue.main) { (_) in
 
-            self.log("willEnterForeground")
+            self.log("UIScene.willEnterForeground")
             self.notify { $0.appWillEnterForeground() }
+        }
+
+        center.addObserver(forName: UIScene.didDisconnectNotification,
+                           object: nil,
+                           queue: OperationQueue.main) { (_) in
+
+            self.log("UIScene.didDisconnect")
+            self.notify { $0.appWillTerminate() }
         }
         #endif
     }
