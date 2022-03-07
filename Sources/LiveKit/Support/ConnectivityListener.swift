bf9b745e8648611d239e7d7abf2ec1f1942822a0
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-03-07 19:24:12 +0900

    update logic

--- Sources/LiveKit/Support/ConnectivityListener.swift
+++ Sources/LiveKit/Support/ConnectivityListener.swift
@@ -12,6 +12,73 @@ internal extension ConnectivityListenerDelegate {
     func connectivityListener(_: ConnectivityListener, didSwitch path: NWPath) {}
 }
 
+internal class ConnectivityListener: MulticastDelegate<ConnectivityListenerDelegate> {
+
+    static let shared = ConnectivityListener()
+
+    public private(set) var hasConnectivity: Bool? {
+        didSet {
+            guard let newValue = hasConnectivity, oldValue != newValue else { return }
+            notify { $0.connectivityListener(self, didUpdate: newValue) }
+        }
+    }
+
+    public private(set) var ipv4: String?
+    public private(set) var path: NWPath?
+
+    private let queue = DispatchQueue(label: "LiveKitSDK.connectivityListener",
+                                      qos: .userInitiated)
+    private let monitor = NWPathMonitor()
+
+    private init() {
+        super.init(qos: .userInitiated)
+
+        log("initial path: \(monitor.currentPath), has: \(monitor.currentPath.hasConnectivity())")
+
+        monitor.pathUpdateHandler = { path in
+            DispatchQueue.sdk.async { self.set(path: path) }
+        }
+
+        monitor.start(queue: queue)
+    }
+}
+
+private extension ConnectivityListener {
+
+    func set(path newValue: NWPath, shouldNotify: Bool = false) {
+
+        log("NWPathDidUpdate status: \(newValue.status), interfaces: \(newValue.availableInterfaces.map({ "\(String(describing: $0.type))-\(String(describing: $0.index))" })), gateways: \(newValue.gateways), activeIp: \(String(describing: newValue.availableInterfaces.first?.ipv4))")
+
+        // check if different path
+        guard newValue != self.path else { return }
+
+        // keep old values
+        let oldValue = self.path
+        let oldIpValue = self.ipv4
+
+        // update new values
+        let newIpValue = newValue.availableInterfaces.first?.ipv4
+        self.path = newValue
+        self.ipv4 = newIpValue
+        self.hasConnectivity = newValue.hasConnectivity()
+
+        // continue if old value exists
+        guard let oldValue = oldValue else { return }
+
+        // continue if was network switch (old and new have connectivity)
+        guard oldValue.hasConnectivity(), newValue.hasConnectivity() else { return }
+
+        let oldInterface = oldValue.availableInterfaces.first
+        let newInterface = newValue.availableInterfaces.first
+
+        if (oldInterface != newInterface) // active interface changed
+            || (oldIpValue != newIpValue) // or, same interface but ip changed (detect wifi network switch)
+        {
+            notify { $0.connectivityListener(self, didSwitch: newValue) }
+        }
+    }
+}
+
 internal extension NWPath {
 
     func hasConnectivity() -> Bool {
@@ -20,51 +87,38 @@ internal extension NWPath {
     }
 }
 
-internal class ConnectivityListener: MulticastDelegate<ConnectivityListenerDelegate> {
+internal extension NWInterface {
 
-    static let shared = ConnectivityListener()
+    func address(family: Int32) -> String? {
 
-    public private(set) var hasConnectivity: Bool {
-        didSet {
-            guard oldValue != hasConnectivity else { return }
-            notify { $0.connectivityListener(self, didUpdate: self.hasConnectivity) }
-        }
-    }
+        var address: String?
 
-    public private(set) var path: NWPath {
-        didSet {
-            defer { self.hasConnectivity = path.hasConnectivity() }
+        // get list of all interfaces on the local machine:
+        var ifaddr: UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0, let firstAddr = ifaddr else { return nil }
 
-            if oldValue.hasConnectivity(), path.hasConnectivity(),
-               // first interface changed
-               (oldValue.availableInterfaces.first != path.availableInterfaces.first)
-                // same interface but gateways changed (detect wifi network switch)
-                || (oldValue.gateways != path.gateways) {
-                notify { $0.connectivityListener(self, didSwitch: self.path) }
+        // for each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(family) {
+                // Check interface name:
+                if name == String(cString: interface.ifa_name) {
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
             }
         }
+        freeifaddrs(ifaddr)
+
+        return address
     }
 
-    private let queue = DispatchQueue(label: "LiveKitSDK.connectivityListener",
-                                      qos: .userInitiated)
-    private let monitor = NWPathMonitor()
-
-    private init() {
-        // set initial values
-        self.path = monitor.currentPath
-        self.hasConnectivity = monitor.currentPath.hasConnectivity()
-
-        super.init(qos: .userInitiated)
-
-        monitor.pathUpdateHandler = { path in
-
-            self.log("NWPathDidUpdate status: \(path.status), interfaces: \(path.availableInterfaces.map({ "\(String(describing: $0.type))-\(String(describing: $0.index))" })), local: \(String(describing: path.localEndpoint)), remote: \(String(describing: path.remoteEndpoint)), gateways: \(path.gateways)")
-
-            DispatchQueue.sdk.async {
-                self.path = path
-            }
-        }
-
-        monitor.start(queue: queue)
-    }
+    var ipv4: String? { self.address(family: AF_INET) }
+    var ipv6: String? { self.address(family: AF_INET6) }
 }
