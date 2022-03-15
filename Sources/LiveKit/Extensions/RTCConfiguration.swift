103df7022beec7a471650675bc21b7a8e9777ce6
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-03-15 10:45:30 +0900

    remove `disableIPV6`

--- Sources/LiveKit/Extensions/RTCConfiguration.swift
+++ Sources/LiveKit/Extensions/RTCConfiguration.swift
@@ -11,7 +11,6 @@ extension RTCConfiguration {
         result.sdpSemantics = .unifiedPlan
         result.continualGatheringPolicy = .gatherContinually
         result.candidateNetworkPolicy = .all
-        result.disableIPV6 = true
         // don't send TCP candidates, they are passive and only server should be sending
         result.tcpCandidatePolicy = .disabled
         result.iceTransportPolicy = .all
