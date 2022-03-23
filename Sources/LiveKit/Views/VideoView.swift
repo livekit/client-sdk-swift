e92fe417aa9329cd732011066a91bb8a6d750ac1
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-03-24 01:56:51 +0900

    videoview deinit

--- Sources/LiveKit/Views/VideoView.swift
+++ Sources/LiveKit/Views/VideoView.swift
@@ -156,6 +156,11 @@ public class VideoView: NativeView, Loggable {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    deinit {
+        log()
+        self.track = nil
+    }
 
     override func shouldLayout() {
         super.shouldLayout()
