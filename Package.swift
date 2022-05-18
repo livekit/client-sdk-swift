07b2e3dfeaf4ff89f15cd7c14c1daaf9f0b011af
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-05-18 22:41:10 +0900

    Use new lib with `shouldReceive` (#88)
    * flip shouldReceive
    
    * use .04
    
    * .05

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("97.4692.02")),
+        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("97.4692.05")),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2")),
