94bc2dac230b6b6724d09a652b245dd82b686f06
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-08-04 02:10:29 +0900

    use .02

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
         )
     ],
     dependencies: [
+        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("104.5112.02")),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2"))
     ],
     targets: [
-        .binaryTarget(name: "WebRTC", path: "WebRTC.framework"),
         .systemLibrary(name: "CHeaders"),
         .target(
             name: "LiveKit",
