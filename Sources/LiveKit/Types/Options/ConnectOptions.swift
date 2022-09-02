ec830bb4ac0eb4a0f8cae04a534e34350162a633
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-09-02 13:59:43 +0900

    ConnectOptions

--- Sources/LiveKit/Types/Options/ConnectOptions.swift
+++ Sources/LiveKit/Types/Options/ConnectOptions.swift
@@ -18,25 +18,39 @@ import Foundation
 import WebRTC
 
 /// Options used when establishing a connection.
-public struct ConnectOptions: Equatable {
+@objc public class ConnectOptions: NSObject {
+
+    public static func == (lhs: ConnectOptions, rhs: ConnectOptions) -> Bool {
+        lhs.autoSubscribe == rhs.autoSubscribe &&
+            lhs.rtcConfiguration == rhs.rtcConfiguration &&
+            lhs.protocolVersion == rhs.protocolVersion  &&
+            lhs.publishOnlyMode == rhs.publishOnlyMode
+    }
 
     /// Automatically subscribe to ``RemoteParticipant``'s tracks.
     /// Defaults to true.
-    public let autoSubscribe: Bool
-    public let rtcConfiguration: RTCConfiguration
+    @objc public let autoSubscribe: Bool
+    @objc public let rtcConfiguration: RTCConfiguration
     /// LiveKit server protocol version to use. Generally, it's not recommended to change this.
-    public let protocolVersion: ProtocolVersion
+    @objc public let protocolVersion: ProtocolVersion
     /// Providing a string will make the connection publish-only, suitable for iOS Broadcast Upload Extensions.
     /// The string can be used to identify the publisher.
-    public let publishOnlyMode: String?
+    @objc public let publishOnlyMode: String?
 
-    public init(autoSubscribe: Bool = true,
-                rtcConfiguration: RTCConfiguration = .liveKitDefault(),
-                publishOnlyMode: String? = nil,
-                protocolVersion: ProtocolVersion = .v8) {
+    @objc public override init() {
+        self.autoSubscribe = true
+        self.rtcConfiguration = .liveKitDefault()
+        self.publishOnlyMode = nil
+        self.protocolVersion = .v8
+    }
+
+    @objc public init(autoSubscribe: Bool = true,
+                      rtcConfiguration: RTCConfiguration? = nil,
+                      publishOnlyMode: String? = nil,
+                      protocolVersion: ProtocolVersion = .v8) {
 
         self.autoSubscribe = autoSubscribe
-        self.rtcConfiguration = rtcConfiguration
+        self.rtcConfiguration = rtcConfiguration ?? .liveKitDefault()
         self.publishOnlyMode = publishOnlyMode
         self.protocolVersion = protocolVersion
     }
