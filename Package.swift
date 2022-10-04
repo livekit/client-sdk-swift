1eec11e061cbae48cd912ec83ef14d8d87a7fb06
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2022-10-04 21:59:31 +0800

    update protos

--- Package.swift
+++ Package.swift
@@ -18,9 +18,9 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("104.5112.02")),
-        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.18.0")),
+        .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.20.2")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
-        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.2"))
+        .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.4"))
     ],
     targets: [
         .systemLibrary(name: "CHeaders"),
