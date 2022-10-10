71db93a318bb4c129e931486b06fe1fc5b0e4aa6
cloudwebrtc <duanweiwei1982@gmail.com>
2022-10-11 07:34:47 +0800

    Testing branch for binary size optimization.

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "LiveKit",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -17,7 +16,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("104.5112.02")),
+        .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs-test.git", .exact("104.5112.04")),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.20.2")),
         .package(name: "Promises", url: "https://github.com/google/promises.git", .upToNextMajor(from: "2.0.0")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.4.4"))
