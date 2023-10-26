980146be2b82d73dbe48fbdfbf3ce8358c51f273
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-10-26 17:38:47 +0900

    WebSocket: Migrate to async/await from Promises lib (#245)
    * progress
    
    * format
    
    * progress
    
    * clean up
    
    * ref
    
    * optimize
    
    * optimize

---
+++ Tests/LiveKitTests/WebSocketTests.swift
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2022 LiveKit
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+@testable import LiveKit
+import XCTest
+
+class WebSocketTests: XCTestCase {
+
+    lazy var socket: WebSocket = {
+        let url = URL(string: "wss://socketsbay.com/wss/v2/1/demo/")!
+        return WebSocket(url: url)
+    }()
+
+    override func setUpWithError() throws {
+
+    }
+
+    override func tearDown() async throws {
+
+    }
+
+    func testCompleter1() async throws {
+        // Read messages
+
+        print("Connecting...")
+        try await socket.connect()
+
+        print("Connected. Waiting for messages...")
+        do {
+            for try await message in socket {
+                switch message {
+                case let .string(string):
+                    print(string)
+                case let .data(data):
+                    print(data)
+                @unknown default:
+                    print("unknown message received")
+                }
+            }
+        } catch {
+            print("Error: \(error)")
+        }
+
+        print("Completed.")
+    }
+}
