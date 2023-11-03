8f842683ce8a663536eea3a41ee4dfe314f4c983
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2023-11-03 21:53:12 +0800

    fix iOS

--- Sources/LiveKit/Broadcast/BroadcastScreenCapturer.swift
+++ Sources/LiveKit/Broadcast/BroadcastScreenCapturer.swift
@@ -16,83 +16,66 @@ import UIKit
 #if os(iOS)
 
 class BroadcastScreenCapturer: BufferCapturer {
+
     static let kRTCScreensharingSocketFD = "rtc_SSFD"
     static let kAppGroupIdentifierKey = "RTCAppGroupIdentifier"
     static let kRTCScreenSharingExtension = "RTCScreenSharingExtension"
 
     var frameReader: SocketConnectionFrameReader?
 
-    override func startCapture() -> Promise<Bool> {
+    override func startCapture() async throws -> Bool {
 
-        super.startCapture().then(on: queue) {didStart -> Promise<Bool> in
+        let didStart = try await super.startCapture()
 
-            guard didStart, self.frameReader == nil else {
-                // already started
-                return Promise(false)
-            }
+        guard didStart else { return false }
 
-            guard let identifier = self.lookUpAppGroupIdentifier(),
-                  let filePath = self.filePathForIdentifier(identifier)
-            else {
-                return Promise { false }
-            }
+        guard let identifier = lookUpAppGroupIdentifier(),
+              let filePath = filePathForIdentifier(identifier) else { return false }
 
-            return Promise { fufill, _ in
-                let bounds = UIScreen.main.bounds
-                let width = bounds.size.width
-                let height = bounds.size.height
-                let screenDimension = Dimensions(width: Int32(width), height: Int32(height))
+        let bounds = await UIScreen.main.bounds
+        let width = bounds.size.width
+        let height = bounds.size.height
+        let screenDimension = Dimensions(width: Int32(width), height: Int32(height))
 
-                // pre fill dimensions, so that we don't have to wait for the broadcast to start to get actual dimensions.
-                // should be able to safely predict using actual screen dimensions.
-                let targetDimensions = screenDimension
-                    .aspectFit(size: self.options.dimensions.max)
-                    .toEncodeSafeDimensions()
+        // pre fill dimensions, so that we don't have to wait for the broadcast to start to get actual dimensions.
+        // should be able to safely predict using actual screen dimensions.
+        let targetDimensions = screenDimension
+            .aspectFit(size: self.options.dimensions.max)
+            .toEncodeSafeDimensions()
 
-                defer { self.dimensions = targetDimensions }
-                let frameReader = SocketConnectionFrameReader()
-                guard let socketConnection = BroadcastServerSocketConnection(filePath: filePath, streamDelegate: frameReader)
-                else {
-                    fufill(false)
-                    return
-                }
-                frameReader.didCapture = { pixelBuffer, rotation in
-                    self.capture(pixelBuffer, rotation: rotation.toLKType())
+        defer { self.dimensions = targetDimensions }
+        let frameReader = SocketConnectionFrameReader()
+        guard let socketConnection = BroadcastServerSocketConnection(filePath: filePath, streamDelegate: frameReader)
+        else { return false }
+        frameReader.didCapture = { pixelBuffer, rotation in
+            self.capture(pixelBuffer, rotation: rotation.toLKType())
 
-                }
-                frameReader.startCapture(with: socketConnection)
-                self.frameReader = frameReader
-                fufill(true)
-            }
         }
+        frameReader.startCapture(with: socketConnection)
+        self.frameReader = frameReader
+
+        return true
     }
 
-    override func stopCapture() -> Promise<Bool> {
+    override func stopCapture() async throws -> Bool {
 
-        super.stopCapture().then(on: queue) { didStop -> Promise<Bool> in
+        let didStop = try await super.stopCapture()
 
-            guard didStop, self.frameReader != nil else {
-                // already stopped
-                return Promise(false)
-            }
+        // Already stopped
+        guard didStop else { return false }
 
-            return Promise { fulfill, _ in
-                self.frameReader?.stopCapture()
-                self.frameReader = nil
-                fulfill(true)
-            }
-        }
+        self.frameReader?.stopCapture()
+        self.frameReader = nil
+        return true
     }
 
     private func lookUpAppGroupIdentifier() -> String? {
-        return Bundle.main.infoDictionary?[BroadcastScreenCapturer.kAppGroupIdentifierKey] as? String
+        Bundle.main.infoDictionary?[BroadcastScreenCapturer.kAppGroupIdentifierKey] as? String
     }
 
     private func filePathForIdentifier(_ identifier: String) -> String? {
         guard let sharedContainer = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: identifier)
-        else {
-            return nil
-        }
+        else { return nil }
 
         let filePath = sharedContainer.appendingPathComponent(BroadcastScreenCapturer.kRTCScreensharingSocketFD).path
         return filePath
