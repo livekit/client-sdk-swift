aa120b137ce1ec43a5a12542d5f462d7cbebe16a
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2024-02-01 00:59:52 +0900

    1.1.7

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -35,7 +35,7 @@ internal let logger = Logger(label: "LiveKitSDK")
 public class LiveKit: NSObject {
 
     @objc(sdkVersion)
-    public static let version = "1.1.6"
+    public static let version = "1.1.7"
 
     @available(*, deprecated, message: "Use Room.connect() instead, protocol v8 and higher do not support this method")
     public static func connect(
