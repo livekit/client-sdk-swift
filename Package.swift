999bfee7f4f77eb4738d645f8b32eecbe376ee3a
hiroshihorie <548776+hiroshihorie@users.noreply.github.com>
2024-02-16 19:58:45 +0900

    use .13

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         // LK-Prefixed Dynamic WebRTC XCFramework
-        .package(name: "LiveKitWebRTC", url: "https://github.com/livekit/webrtc-xcframework.git", .exact("114.5735.12")),
+        .package(name: "LiveKitWebRTC", url: "https://github.com/livekit/webrtc-xcframework.git", .exact("114.5735.13")),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", .upToNextMajor(from: "1.25.2")),
         .package(url: "https://github.com/apple/swift-log.git", .upToNextMajor(from: "1.5.3")),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
