f523c8d368f25fc6e0539a25b0cfe2b23bfabf0c
hiroshihorie <548776+hiroshihorie@users.noreply.github.com>
2024-03-07 04:58:55 +0900

    add test

--- Tests/LiveKitTests/SerialRunnerActor.swift
+++ Tests/LiveKitTests/SerialRunnerActor.swift
@@ -18,8 +18,9 @@
 import XCTest
 
 class AsyncSerialExecutorTests: XCTestCase {
-    var serialExecutor1Counter: Int = 0
-    let serialExecutor1 = SerialRunnerActor<Void>()
+    let serialRunner = SerialRunnerActor<Void>()
+    var counterValue: Int = 0
+    var resultValues: [String] = []
 
     // Test whether tasks, when invoked concurrently, continue to run in a serial manner.
     // Access to the counter value should be synchronized, aiming for a final count of 0.
@@ -28,14 +29,14 @@ class AsyncSerialExecutorTests: XCTestCase {
         try await withThrowingTaskGroup(of: Void.self) { group in
             for i in 1 ... 1000 {
                 group.addTask {
-                    try await self.serialExecutor1.run {
-                        self.serialExecutor1Counter += 1
+                    try await self.serialRunner.run {
+                        self.counterValue += 1
                         let ns = UInt64(Double.random(in: 1 ..< 3) * 1_000_000)
                         print("executor1 task \(i) start, will wait \(ns)ns")
                         // Simulate random-ish time consuming task
                         try await Task.sleep(nanoseconds: ns)
                         print("executor1 task \(i) done")
-                        self.serialExecutor1Counter -= 1
+                        self.counterValue -= 1
                     }
                 }
             }
@@ -43,9 +44,9 @@ class AsyncSerialExecutorTests: XCTestCase {
             try await group.waitForAll()
         }
 
-        print("serialExecutor1Counter: \(serialExecutor1Counter)")
+        print("serialExecutor1Counter: \(counterValue)")
         // Should end up being 0
-        XCTAssert(serialExecutor1Counter == 0)
+        XCTAssert(counterValue == 0)
     }
 
     // Test whether tasks invoked concurrently, and randomly cancelled, continue to run in a serial manner.
@@ -57,12 +58,12 @@ class AsyncSerialExecutorTests: XCTestCase {
                 group.addTask {
                     let subTask = Task {
                         do {
-                            try await self.serialExecutor1.run {
+                            try await self.serialRunner.run {
                                 // Increment counter
-                                self.serialExecutor1Counter += 1
+                                self.counterValue += 1
                                 defer {
                                     // Decrement counter
-                                    self.serialExecutor1Counter -= 1
+                                    self.counterValue -= 1
                                 }
 
                                 let ns = UInt64(Double.random(in: 1 ..< 3) * 1_000_000)
@@ -91,8 +92,60 @@ class AsyncSerialExecutorTests: XCTestCase {
             await group.waitForAll()
         }
 
-        print("serialExecutor1Counter: \(serialExecutor1Counter)")
+        print("serialExecutor1Counter: \(counterValue)")
         // Should end up being 0
-        XCTAssert(serialExecutor1Counter == 0)
+        XCTAssert(counterValue == 0)
+    }
+
+    func testSerialExecutionOrderWithCancel() async throws {
+        // Run Tasks concurrently
+        try await withThrowingTaskGroup(of: Void.self) { group in
+
+            // Schedule task 1
+            print("Scheduling task 1...")
+            group.addTask {
+                let subTask = Task {
+                    do {
+                        try await self.serialRunner.run {
+                            defer {
+                                self.resultValues.append("task1")
+                                print("task 1 done")
+                            }
+                            // Simulate a long task
+                            let ns = UInt64(3 * 1_000_000_000)
+                            print("task 1 waiting \(ns)ns...")
+                            // Simulate random-ish time consuming task
+                            try await Task.sleep(nanoseconds: ns)
+                        }
+                    } catch {
+                        // Handle exceptions so test will continue
+                        print("Task 1 throwed \(error)")
+                    }
+                }
+
+                // Cancel after 1.5 second
+                try await Task.sleep(nanoseconds: UInt64(1.5 * 1_000_000_000))
+                print("Cancelling task 1...")
+                subTask.cancel()
+
+                return await subTask.value
+            }
+
+            // Schedule task 2 after 1 second
+            try await Task.sleep(nanoseconds: UInt64(1 * 1_000_000_000))
+            print("Scheduling task 2...")
+            group.addTask {
+                try await self.serialRunner.run {
+                    self.resultValues.append("task2")
+                    print("task 2 done")
+                }
+            }
+
+            try await group.waitForAll()
+        }
+
+        print("completed tasks order: \(resultValues)")
+        // Should be in order
+        XCTAssertEqual(resultValues, ["task1", "task2"])
     }
 }
