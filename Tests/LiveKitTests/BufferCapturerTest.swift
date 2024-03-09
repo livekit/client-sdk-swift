57399f8b17e5b85b53b89b80732eac361356c05f
hiroshihorie <548776+hiroshihorie@users.noreply.github.com>
2024-03-09 17:15:18 +0900

    organize

--- Tests/LiveKitTests/BufferCapturerTest.swift
+++ Tests/LiveKitTests/BufferCapturerTest.swift
@@ -20,55 +20,6 @@ import XCTest
 
 @available(iOS 15.0, *)
 class BufferCapturerTest: XCTestCase {
-    // Creates a LocalVideoTrack with BufferCapturer, generates frames for approx 30 seconds
-    func createSampleVideoTrack(targetFps: Int = 30, _ onCapture: @escaping (CMSampleBuffer) -> Void) async throws -> (Task<Void, any Error>) {
-        // Sample video
-        let url = URL(string: "https://storage.unxpected.co.jp/public/sample-videos/ocean-1080p.mp4")!
-
-        print("Downloading sample video from \(url)...")
-        // TODO: Backport for iOS13
-        let (downloadedLocalUrl, _) = try await URLSession.shared.download(from: url)
-
-        // Move the file to a new temporary location with a more descriptive name, if desired
-        let tempLocalUrl = FileManager.default.temporaryDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("mp4")
-        try FileManager.default.moveItem(at: downloadedLocalUrl, to: tempLocalUrl)
-
-        print("Opening \(tempLocalUrl) with asset reader...")
-        let asset = AVAsset(url: tempLocalUrl)
-        let assetReader = try AVAssetReader(asset: asset)
-
-        guard let track = asset.tracks(withMediaType: .video).first else {
-            XCTFail("No video track found in sample video file")
-            fatalError()
-        }
-
-        let outputSettings: [String: Any] = [
-            kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA),
-        ]
-
-        let trackOutput = AVAssetReaderTrackOutput(track: track, outputSettings: outputSettings)
-        assetReader.add(trackOutput)
-
-        // Start reading...
-        guard assetReader.startReading() else {
-            XCTFail("Could not start reading the asset.")
-            fatalError()
-        }
-
-        // XCTAssert(assetReader.status == .reading)
-
-        let readBufferTask = Task.detached {
-            let frameDuration = UInt64(1_000_000_000 / targetFps)
-            while !Task.isCancelled, assetReader.status == .reading, let sampleBuffer = trackOutput.copyNextSampleBuffer() {
-                onCapture(sampleBuffer)
-                // Sleep for the frame duration to regulate to ~30 fps
-                try await Task.sleep(nanoseconds: frameDuration)
-            }
-        }
-
-        return readBufferTask
-    }
-
     func testPublishBufferTrack() async throws {
         try await with2Rooms { room1, room2 in
 
@@ -130,69 +81,5 @@ class BufferCapturerTest: XCTestCase {
             // Clean up
             watchParticipant.cancel()
         }
-    }
-}
-
-class VideoTrackWatcher: TrackDelegate, VideoRenderer {
-    // MARK: - Public
-
-    typealias OnDidRenderFirstFrame = (_ sid: String) -> Void
-    public var didRenderFirstFrame: Bool { _state.didRenderFirstFrame }
-
-    private struct State {
-        var didRenderFirstFrame: Bool = false
-    }
-
-    public let id: String
-    private let _state = StateSync(State())
-    private let onDidRenderFirstFrame: OnDidRenderFirstFrame?
-
-    init(id: String, onDidRenderFirstFrame: OnDidRenderFirstFrame? = nil) {
-        self.id = id
-        self.onDidRenderFirstFrame = onDidRenderFirstFrame
-    }
-
-    public func reset() {
-        _state.mutate { $0.didRenderFirstFrame = false }
-    }
-
-    // MARK: - VideoRenderer
-
-    var isAdaptiveStreamEnabled: Bool { true }
-
-    var adaptiveStreamSize: CGSize { .init(width: 1920, height: 1080) }
-
-    func set(size: CGSize) {
-        print("\(type(of: self)) set(size: \(size))")
-    }
-
-    func render(frame _: LiveKit.VideoFrame) {
-        _state.mutate {
-            if !$0.didRenderFirstFrame {
-                $0.didRenderFirstFrame = true
-                onDidRenderFirstFrame?(id)
-            }
-        }
-    }
-
-    // MARK: - TrackDelegate
-
-    func track(_: Track, didUpdateStatistics statistics: TrackStatistics, simulcastStatistics _: [VideoCodec: TrackStatistics]) {
-        guard let stream = statistics.inboundRtpStream.first else { return }
-        var segments: [String] = []
-
-        if let codec = statistics.codec.first(where: { $0.id == stream.codecId }), let mimeType = codec.mimeType {
-            segments.append("codec: \(mimeType)")
-        }
-
-        if let width = stream.frameWidth, let height = stream.frameHeight {
-            segments.append("dimensions: \(width)x\(height)")
-        }
-
-        if let fps = stream.framesPerSecond {
-            segments.append("fps: \(fps)")
-        }
-
-        print("\(type(of: self)) didUpdateStatistics (\(segments.joined(separator: ", ")))")
     }
 }
