9dbc11ac8071a949e913cdbd8d36a3e8db1b15e7
hiroshihorie <548776+hiroshihorie@users.noreply.github.com>
2024-05-31 14:48:08 +0900

    v2.0.9

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -34,7 +34,7 @@ let logger = Logger(label: "LiveKitSDK")
 @objc
 public class LiveKitSDK: NSObject {
     @objc(sdkVersion)
-    public static let version = "2.0.8"
+    public static let version = "2.0.9"
 
     @objc
     public static func setLoggerStandardOutput() {
