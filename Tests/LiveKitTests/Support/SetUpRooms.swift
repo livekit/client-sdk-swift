fd400605cc60c6bda94fbbeae760b7d53075350f
hiroshihorie <548776+hiroshihorie@users.noreply.github.com>
2024-06-06 18:29:19 +0900

    RoomTestingOptions

--- Tests/LiveKitTests/Support/SetUpRooms.swift
+++ Tests/LiveKitTests/Support/SetUpRooms.swift
@@ -17,6 +17,21 @@
 @testable import LiveKit
 import XCTest
 
+struct RoomTestingOptions {
+    let delegate: RoomDelegate?
+    let canPublish: Bool
+    let canSubscribe: Bool
+
+    init(delegate: RoomDelegate? = nil,
+         canPublish: Bool = false,
+         canSubscribe: Bool = false)
+    {
+        self.delegate = delegate
+        self.canPublish = canPublish
+        self.canSubscribe = canSubscribe
+    }
+}
+
 extension XCTestCase {
     private func readEnvironmentString(for key: String, defaultValue: String) -> String {
         if let string = ProcessInfo.processInfo.environment[key]?.trimmingCharacters(in: .whitespacesAndNewlines), !string.isEmpty {
@@ -30,7 +45,11 @@ extension XCTestCase {
         readEnvironmentString(for: "LIVEKIT_TESTING_URL", defaultValue: "ws://localhost:7880")
     }
 
-    func liveKitServerToken(for room: String, identity: String) throws -> String {
+    func liveKitServerToken(for room: String,
+                            identity: String,
+                            canPublish: Bool,
+                            canSubscribe: Bool) throws -> String
+    {
         let apiKey = readEnvironmentString(for: "LIVEKIT_TESTING_API_KEY", defaultValue: "devkey")
         let apiSecret = readEnvironmentString(for: "LIVEKIT_TESTING_API_SECRET", defaultValue: "secret")
 
@@ -40,31 +59,41 @@ extension XCTestCase {
 
         tokenGenerator.videoGrant = VideoGrant(room: room,
                                                roomJoin: true,
-                                               canPublish: true)
+                                               canPublish: canPublish,
+                                               canSubscribe: canSubscribe)
         return try tokenGenerator.sign()
     }
 
     // Set up 2 Rooms
-    func with2Rooms(delegate1: RoomDelegate? = nil,
-                    delegate2: RoomDelegate? = nil,
+    func with2Rooms(room1 room1Options: RoomTestingOptions = RoomTestingOptions(),
+                    room2 room2Options: RoomTestingOptions = RoomTestingOptions(),
+                    e2eeKey: String = UUID().uuidString,
                     _ block: @escaping (Room, Room) async throws -> Void) async throws
     {
-        let e2eeKey = UUID().uuidString
         let e2eeOptions = E2EEOptions(keyProvider: BaseKeyProvider(isSharedKey: true, sharedKey: e2eeKey))
 
         // Turn on stats
         let roomOptions = RoomOptions(e2eeOptions: e2eeOptions, reportRemoteTrackStatistics: true)
 
-        let room1 = Room(delegate: delegate1, roomOptions: roomOptions)
-        let room2 = Room(delegate: delegate2, roomOptions: roomOptions)
+        let room1 = Room(delegate: room1Options.delegate, roomOptions: roomOptions)
+        let room2 = Room(delegate: room2Options.delegate, roomOptions: roomOptions)
 
         let url = liveKitServerUrl()
         print("url: \(url)")
 
         let roomName = UUID().uuidString
 
-        let token1 = try liveKitServerToken(for: roomName, identity: "identity01")
-        let token2 = try liveKitServerToken(for: roomName, identity: "identity02")
+        let token1 = try liveKitServerToken(for: roomName,
+                                            identity: "identity01",
+                                            canPublish: room1Options.canPublish,
+                                            canSubscribe: room1Options.canSubscribe)
+        print("Token: \(token1) for room: \(roomName)")
+
+        let token2 = try liveKitServerToken(for: roomName,
+                                            identity: "identity02",
+                                            canPublish: room2Options.canPublish,
+                                            canSubscribe: room2Options.canSubscribe)
+        print("Token: \(token2) for room: \(roomName)")
 
         try await withThrowingTaskGroup(of: Void.self) { group in
             group.addTask {
@@ -77,7 +106,11 @@ extension XCTestCase {
             try await group.waitForAll()
         }
 
-        let observerToken = try liveKitServerToken(for: roomName, identity: "observer")
+        let observerToken = try liveKitServerToken(for: roomName,
+                                                   identity: "observer",
+                                                   canPublish: true,
+                                                   canSubscribe: true)
+
         print("Observer token: \(observerToken) for room: \(roomName)")
 
         let room1ParticipantCountIs2 = expectation(description: "Room1 Participant count is 2")
@@ -87,13 +120,13 @@ extension XCTestCase {
         room2ParticipantCountIs2.assertForOverFulfill = false
 
         let watchRoom1 = room1.objectWillChange.sink { _ in
-            if room1.allParticipants.count == 2 {
+            if room1.allParticipants.count >= 2 {
                 room1ParticipantCountIs2.fulfill()
             }
         }
 
         let watchRoom2 = room2.objectWillChange.sink { _ in
-            if room2.allParticipants.count == 2 {
+            if room2.allParticipants.count >= 2 {
                 room2ParticipantCountIs2.fulfill()
             }
         }
