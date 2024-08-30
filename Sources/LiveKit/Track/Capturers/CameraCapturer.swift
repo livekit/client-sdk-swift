a8b2af15b7994869905a27622ab82f205e965a57
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2024-08-30 13:53:40 +0900

    tvOS: Enable Multicam & fix device enumeration (#471)

--- Sources/LiveKit/Track/Capturers/CameraCapturer.swift
+++ Sources/LiveKit/Track/Capturers/CameraCapturer.swift
@@ -154,8 +154,8 @@ public class CameraCapturer: VideoCapturer {
         var device: AVCaptureDevice? = options.device
 
         if device == nil {
-            #if os(iOS)
-            let devices: [AVCaptureDevice]
+            #if os(iOS) || os(tvOS)
+            var devices: [AVCaptureDevice]
             if AVCaptureMultiCamSession.isMultiCamSupported {
                 // Get the list of devices already on the shared multi-cam session.
                 let existingDevices = captureSession.inputs.compactMap { $0 as? AVCaptureDeviceInput }.map(\.device)
@@ -194,7 +194,7 @@ public class CameraCapturer: VideoCapturer {
             // Use the preferred capture format if specified in options
             selectedFormat = foundFormat
         } else {
-            if let foundFormat = sortedFormats.first(where: { $0.dimensions.area >= self.options.dimensions.area && $0.format.fpsRange().contains(self.options.fps) && $0.format.isMultiCamSupportediOS }) {
+            if let foundFormat = sortedFormats.first(where: { $0.dimensions.area >= self.options.dimensions.area && $0.format.fpsRange().contains(self.options.fps) && $0.format.filterForMulticamSupport }) {
                 // Use the first format that satisfies preferred dimensions & fps
                 selectedFormat = foundFormat
             } else if let foundFormat = sortedFormats.first(where: { $0.dimensions.area >= self.options.dimensions.area }) {
@@ -322,8 +322,10 @@ extension AVCaptureDevice.Format {
         }
     }
 
-    var isMultiCamSupportediOS: Bool {
-        #if os(iOS)
+    // Used for filtering.
+    // Only include multi-cam supported devices if in multi-cam mode. Otherwise, always include the devices.
+    var filterForMulticamSupport: Bool {
+        #if os(iOS) || os(tvOS)
         return AVCaptureMultiCamSession.isMultiCamSupported ? isMultiCamSupported : true
         #else
         return true
