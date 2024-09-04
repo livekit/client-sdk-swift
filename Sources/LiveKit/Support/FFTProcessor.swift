a5ba133f4be76f6e63118545b8d7fffe9b3aba1b
hiroshihorie <548776+hiroshihorie@users.noreply.github.com>
2024-09-04 15:31:49 +0900

    Optimize

--- Sources/LiveKit/Support/FFTProcessor.swift
+++ Sources/LiveKit/Support/FFTProcessor.swift
@@ -13,17 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import Accelerate
 import Foundation
 
 extension Float {
-    /// The Nyquist frequency is sampleRate / 2.
     var nyquistFrequency: Float { self / 2.0 }
 
     var toDecibels: Float {
-        // Avoid log of zero or negative values by using a very small value.
-        let minMagnitude: Float = 0.0000001
+        let minMagnitude: Float = 1e-7
         return 20 * log10(max(magnitude, minMagnitude))
     }
 }
@@ -35,23 +32,14 @@ public struct FFTComputeBandsResult {
 }
 
 public class FFTResult {
-    // Result of fft operation.
     public let magnitudes: [Float]
 
     init(magnitudes: [Float]) {
         self.magnitudes = magnitudes
     }
 
-    // MARK: - Public
-
-    /// Applies logical banding on top of the spectrum data. The bands are spaced linearly throughout the spectrum.
-    func computeBands(minFrequency: Float,
-                      maxFrequency: Float,
-                      bandsCount: Int,
-                      sampleRate: Float) -> FFTComputeBandsResult
-    {
+    func computeBands(minFrequency: Float, maxFrequency: Float, bandsCount: Int, sampleRate: Float) -> FFTComputeBandsResult {
         let actualMaxFrequency = min(sampleRate.nyquistFrequency, maxFrequency)
-
         var bandMagnitudes = [Float](repeating: 0.0, count: bandsCount)
         var bandFrequencies = [Float](repeating: 0.0, count: bandsCount)
 
@@ -62,23 +50,16 @@ public class FFTResult {
         for i in 0 ..< bandsCount {
             let magsStartIdx = Int(floorf(Float(i) * ratio)) + magLowerRange
             let magsEndIdx = Int(floorf(Float(i + 1) * ratio)) + magLowerRange
-            var magsAvg: Float
-            if magsEndIdx == magsStartIdx {
-                // Can happen when numberOfBands < # of magnitudes. No need to average anything.
-                magsAvg = magnitudes[magsStartIdx]
-            } else {
-                magsAvg = _computeAverage(magnitudes, magsStartIdx, magsEndIdx)
-            }
-            bandMagnitudes[i] = magsAvg
+
+            bandMagnitudes[i] = magsEndIdx == magsStartIdx
+                ? magnitudes[magsStartIdx]
+                : _computeAverage(magnitudes, magsStartIdx, magsEndIdx)
+
             bandFrequencies[i] = _averageFrequencyInRange(magsStartIdx, magsEndIdx, sampleRate: sampleRate)
         }
 
-        return FFTComputeBandsResult(count: bandsCount,
-                                     magnitudes: bandMagnitudes,
-                                     frequencies: bandFrequencies)
+        return FFTComputeBandsResult(count: bandsCount, magnitudes: bandMagnitudes, frequencies: bandFrequencies)
     }
-
-    // MARK: - Private
 
     @inline(__always) private func _magnitudeIndex(for frequency: Float, sampleRate: Float) -> Int {
         Int(Float(magnitudes.count) * frequency / sampleRate.nyquistFrequency)
@@ -86,15 +67,15 @@ public class FFTResult {
 
     @inline(__always) private func _computeAverage(_ array: [Float], _ startIdx: Int, _ stopIdx: Int) -> Float {
         var mean: Float = 0
+        let count = stopIdx - startIdx
         array.withUnsafeBufferPointer { bufferPtr in
             let ptr = bufferPtr.baseAddress! + startIdx
-            vDSP_meanv(ptr, 1, &mean, UInt(stopIdx - startIdx))
+            vDSP_meanv(ptr, 1, &mean, UInt(count))
         }
         return mean
     }
 
-    /// The average bandwidth throughout the spectrum (nyquist / magnitudes.count)
-    @inline(__always) func _computeBandwidth(for sampleRate: Float) -> Float {
+    @inline(__always) private func _computeBandwidth(for sampleRate: Float) -> Float {
         sampleRate.nyquistFrequency / Float(magnitudes.count)
     }
 
@@ -105,8 +86,6 @@ public class FFTResult {
 }
 
 class FFTProcessor {
-    // MARK: - Public
-
     public enum WindowType {
         case none
         case hanning
@@ -114,73 +93,51 @@ class FFTProcessor {
     }
 
     public let bufferSize: Int
-
-    /// Supplying a window type (hanning or hamming) smooths the edges of the incoming waveform and reduces output errors from the FFT function.
-    /// https://en.wikipedia.org/wiki/Spectral_leakage
     public let windowType: WindowType
-
-    // MARK: - Private
 
     private let bufferHalfSize: Int
     private let bufferLog2Size: Int
     private var window: [Float] = []
     private var fftSetup: FFTSetup
-
-    private var complexBuffer: DSPSplitComplex!
+    private var complexBuffer: DSPSplitComplex
     private var realPointer: UnsafeMutablePointer<Float>
     private var imaginaryPointer: UnsafeMutablePointer<Float>
 
-    init(bufferSize inBufferSize: Int, windowType: WindowType = .hamming) {
-        bufferSize = inBufferSize
+    init(bufferSize: Int, windowType: WindowType = .hamming) {
+        self.bufferSize = bufferSize
         self.windowType = windowType
-        bufferHalfSize = inBufferSize / 2
+        bufferHalfSize = bufferSize / 2
+        bufferLog2Size = Int(log2f(Float(bufferSize)))
 
-        let bufferSizeFloat = Float(inBufferSize)
-
-        // bufferSize must be a power of 2.
-        let lg2 = logbf(bufferSizeFloat)
-        assert(remainderf(bufferSizeFloat, powf(2.0, lg2)) == 0, "bufferSize must be a power of 2")
-        bufferLog2Size = Int(log2f(bufferSizeFloat))
-
-        // Create fft setup.
         fftSetup = vDSP_create_fftsetup(UInt(bufferLog2Size), FFTRadix(FFT_RADIX2))!
 
-        // Allocate memory for the real and imaginary parts.
-        realPointer = UnsafeMutablePointer<Float>.allocate(capacity: bufferHalfSize)
-        imaginaryPointer = UnsafeMutablePointer<Float>.allocate(capacity: bufferHalfSize)
+        realPointer = .allocate(capacity: bufferHalfSize)
+        imaginaryPointer = .allocate(capacity: bufferHalfSize)
 
-        // Initialize the memory to zero.
         realPointer.initialize(repeating: 0.0, count: bufferHalfSize)
         imaginaryPointer.initialize(repeating: 0.0, count: bufferHalfSize)
 
-        // Init the complexBuffer.
         complexBuffer = DSPSplitComplex(realp: realPointer, imagp: imaginaryPointer)
     }
 
     deinit {
-        // destroy the fft setup object
         vDSP_destroy_fftsetup(fftSetup)
-
         realPointer.deallocate()
         imaginaryPointer.deallocate()
     }
 
     func process(buffer: [Float]) -> FFTResult {
-        // Ensure the input buffer is the correct size (twice the half buffer size, since it is interleaved).
         guard buffer.count == bufferSize else {
-            fatalError("Input buffer size does not match the initialized buffer size.")
+            fatalError("Input buffer size mismatch.")
         }
 
-        // Convert the interleaved real and imaginary parts to a split complex form.
         buffer.withUnsafeBufferPointer { bufferPtr in
             let complexPtr = UnsafeRawPointer(bufferPtr.baseAddress!).bindMemory(to: DSPComplex.self, capacity: bufferHalfSize)
             vDSP_ctoz(complexPtr, 2, &complexBuffer, 1, UInt(bufferHalfSize))
         }
 
-        // Perform a forward FFT.
         vDSP_fft_zrip(fftSetup, &complexBuffer, 1, UInt(bufferLog2Size), Int32(FFT_FORWARD))
 
-        // Calculate magnitudes.
         var magnitudes = [Float](repeating: 0.0, count: bufferHalfSize)
         vDSP_zvmags(&complexBuffer, 1, &magnitudes, 1, UInt(bufferHalfSize))
 
