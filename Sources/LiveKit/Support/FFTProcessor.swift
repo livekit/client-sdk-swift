e5c6df20f79ff0579b39070bca2fa60e58908c59
hiroshihorie <548776+hiroshihorie@users.noreply.github.com>
2024-10-03 11:45:01 +0900

    Optimize

--- Sources/LiveKit/Support/FFTProcessor.swift
+++ Sources/LiveKit/Support/FFTProcessor.swift
@@ -50,49 +50,33 @@ public class FFTResult {
         let magUpperRange = _magnitudeIndex(for: actualMaxFrequency, sampleRate: sampleRate)
         let ratio = Float(magUpperRange - magLowerRange) / Float(bandsCount)
 
-        for i in 0 ..< bandsCount {
-            let magsStartIdx = Int(floorf(Float(i) * ratio)) + magLowerRange
-            let magsEndIdx = Int(floorf(Float(i + 1) * ratio)) + magLowerRange
+        return magnitudes.withUnsafeBufferPointer { magnitudesPtr in
+            for i in 0 ..< bandsCount {
+                let magsStartIdx = vDSP_Length(floorf(Float(i) * ratio)) + magLowerRange
+                let magsEndIdx = vDSP_Length(floorf(Float(i + 1) * ratio)) + magLowerRange
 
-            let count = magsEndIdx - magsStartIdx
-            if count > 0 {
-                if scaleType == .linear {
-                    // Linear scale averaging
-                    bandMagnitudes[i] = _computeAverage(magnitudes, magsStartIdx, magsEndIdx)
+                let count = magsEndIdx - magsStartIdx
+                if count > 0 {
+                    if scaleType == .linear {
+                        var sum: Float = 0
+                        vDSP_sve(magnitudesPtr.baseAddress! + Int(magsStartIdx), 1, &sum, count)
+                        bandMagnitudes[i] = sum / Float(count)
+                    }
+                } else {
+                    bandMagnitudes[i] = magnitudes[Int(magsStartIdx)]
                 }
-            } else {
-                // Single value case
-                bandMagnitudes[i] = magnitudes[magsStartIdx]
+
+                // Compute average frequency
+                let bandwidth = sampleRate.nyquistFrequency / Float(magnitudes.count)
+                bandFrequencies[i] = (bandwidth * Float(magsStartIdx) + bandwidth * Float(magsEndIdx)) / 2
             }
 
-            // Compute average frequency
-            bandFrequencies[i] = _averageFrequencyInRange(magsStartIdx, magsEndIdx, sampleRate: sampleRate)
+            return FFTComputeBandsResult(count: bandsCount, magnitudes: bandMagnitudes, frequencies: bandFrequencies)
         }
-
-        return FFTComputeBandsResult(count: bandsCount, magnitudes: bandMagnitudes, frequencies: bandFrequencies)
     }
 
-    @inline(__always) private func _magnitudeIndex(for frequency: Float, sampleRate: Float) -> Int {
-        Int(Float(magnitudes.count) * frequency / sampleRate.nyquistFrequency)
-    }
-
-    @inline(__always) private func _computeAverage(_ array: [Float], _ startIdx: Int, _ stopIdx: Int) -> Float {
-        var mean: Float = 0
-        let count = stopIdx - startIdx
-        array.withUnsafeBufferPointer { bufferPtr in
-            let ptr = bufferPtr.baseAddress! + startIdx
-            vDSP_meanv(ptr, 1, &mean, UInt(count))
-        }
-        return mean
-    }
-
-    @inline(__always) private func _computeBandwidth(for sampleRate: Float) -> Float {
-        sampleRate.nyquistFrequency / Float(magnitudes.count)
-    }
-
-    @inline(__always) private func _averageFrequencyInRange(_ startIndex: Int, _ endIndex: Int, sampleRate: Float) -> Float {
-        let bandwidth = _computeBandwidth(for: sampleRate)
-        return (bandwidth * Float(startIndex) + bandwidth * Float(endIndex)) / 2
+    @inline(__always) private func _magnitudeIndex(for frequency: Float, sampleRate: Float) -> vDSP_Length {
+        vDSP_Length(Float(magnitudes.count) * frequency / sampleRate.nyquistFrequency)
     }
 }
 
@@ -108,81 +92,69 @@ class FFTProcessor {
         case logarithmic
     }
 
-    public let bufferSize: Int
+    public let bufferSize: vDSP_Length
     public let windowType: WindowType
     public let scaleType: ScaleType
 
-    private let bufferHalfSize: Int
-    private let bufferLog2Size: Int
+    private let bufferHalfSize: vDSP_Length
+    private let bufferLog2Size: vDSP_Length
     private var window: [Float] = []
     private var fftSetup: FFTSetup
-    private var complexBuffer: DSPSplitComplex
-    private var realPointer: UnsafeMutablePointer<Float>
-    private var imaginaryPointer: UnsafeMutablePointer<Float>
+    private var realBuffer: [Float]
+    private var imaginaryBuffer: [Float]
     private var zeroDBReference: Float = 1.0
 
     init(bufferSize: Int, scaleType: ScaleType = .linear, windowType: WindowType = .hanning) {
-        self.bufferSize = bufferSize
+        self.bufferSize = vDSP_Length(bufferSize)
         self.scaleType = scaleType
         self.windowType = windowType
 
-        bufferHalfSize = bufferSize / 2
-        bufferLog2Size = Int(log2f(Float(bufferSize)))
+        bufferHalfSize = vDSP_Length(bufferSize / 2)
+        bufferLog2Size = vDSP_Length(log2f(Float(bufferSize)))
+
+        realBuffer = [Float](repeating: 0.0, count: Int(bufferHalfSize))
+        imaginaryBuffer = [Float](repeating: 0.0, count: Int(bufferHalfSize))
+        window = [Float](repeating: 1.0, count: Int(bufferSize))
 
         fftSetup = vDSP_create_fftsetup(UInt(bufferLog2Size), FFTRadix(FFT_RADIX2))!
 
-        realPointer = .allocate(capacity: bufferHalfSize)
-        imaginaryPointer = .allocate(capacity: bufferHalfSize)
-
-        realPointer.initialize(repeating: 0.0, count: bufferHalfSize)
-        imaginaryPointer.initialize(repeating: 0.0, count: bufferHalfSize)
-
-        complexBuffer = DSPSplitComplex(realp: realPointer, imagp: imaginaryPointer)
-        setupWindow()
-    }
-
-    deinit {
-        vDSP_destroy_fftsetup(fftSetup)
-        realPointer.deallocate()
-        imaginaryPointer.deallocate()
-    }
-
-    private func setupWindow() {
-        window = [Float](repeating: 1.0, count: bufferSize)
         switch windowType {
         case .none:
             break
         case .hanning:
-            vDSP_hann_window(&window, UInt(bufferSize), Int32(vDSP_HANN_NORM))
+            vDSP_hann_window(&window, vDSP_Length(bufferSize), Int32(vDSP_HANN_NORM))
         case .hamming:
-            vDSP_hamm_window(&window, UInt(bufferSize), 0)
+            vDSP_hamm_window(&window, vDSP_Length(bufferSize), 0)
         }
     }
 
+    deinit {
+        vDSP_destroy_fftsetup(fftSetup)
+    }
+
     func process(buffer: [Float]) -> FFTResult {
-        guard buffer.count == bufferSize else {
-            fatalError("Input buffer size mismatch.")
+        precondition(buffer.count == Int(bufferSize), "Input buffer size mismatch.")
+
+        var windowedBuffer = [Float](repeating: 0.0, count: Int(bufferSize))
+
+        vDSP_vmul(buffer, 1, window, 1, &windowedBuffer, 1, bufferSize)
+
+        return realBuffer.withUnsafeMutableBufferPointer { realPtr in
+            imaginaryBuffer.withUnsafeMutableBufferPointer { imagPtr in
+                var complexBuffer = DSPSplitComplex(realp: realPtr.baseAddress!, imagp: imagPtr.baseAddress!)
+
+                windowedBuffer.withUnsafeBufferPointer { bufferPtr in
+                    let complexPtr = UnsafeRawPointer(bufferPtr.baseAddress!).bindMemory(to: DSPComplex.self, capacity: Int(bufferHalfSize))
+                    vDSP_ctoz(complexPtr, 2, &complexBuffer, 1, bufferHalfSize)
+                }
+
+                vDSP_fft_zrip(fftSetup, &complexBuffer, 1, bufferLog2Size, FFTDirection(FFT_FORWARD))
+
+                var magnitudes = [Float](repeating: 0.0, count: Int(bufferHalfSize))
+                vDSP_zvabs(&complexBuffer, 1, &magnitudes, 1, bufferHalfSize)
+
+                return FFTResult(magnitudes: magnitudes, scaleType: scaleType)
+            }
         }
-
-        // Create a new array to hold the windowed buffer
-        var windowedBuffer = [Float](repeating: 0.0, count: bufferSize)
-
-        // Multiply the input buffer by the window coefficients
-        vDSP_vmul(buffer, 1, window, 1, &windowedBuffer, 1, UInt(bufferSize))
-
-        // Convert the real input to split complex form
-        windowedBuffer.withUnsafeBufferPointer { bufferPtr in
-            let complexPtr = UnsafeRawPointer(bufferPtr.baseAddress!).bindMemory(to: DSPComplex.self, capacity: bufferHalfSize)
-            vDSP_ctoz(complexPtr, 2, &complexBuffer, 1, UInt(bufferHalfSize))
-        }
-
-        // Perform the FFT
-        vDSP_fft_zrip(fftSetup, &complexBuffer, 1, UInt(bufferLog2Size), Int32(FFT_FORWARD))
-
-        // Calculate magnitudes
-        var magnitudes = [Float](repeating: 0.0, count: bufferHalfSize)
-        vDSP_zvabs(&complexBuffer, 1, &magnitudes, 1, UInt(bufferHalfSize))
-
-        return FFTResult(magnitudes: magnitudes, scaleType: scaleType)
     }
 }
