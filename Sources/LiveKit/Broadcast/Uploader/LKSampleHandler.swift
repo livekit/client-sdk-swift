dd216106d45284e0de71893f4a4caf38fad9bd7f
Ben Cherry <ben@livekit.io>
2024-12-03 23:20:50 -0800

    wip

--- Sources/LiveKit/Broadcast/Uploader/LKSampleHandler.swift
+++ Sources/LiveKit/Broadcast/Uploader/LKSampleHandler.swift
@@ -57,6 +57,8 @@ open class LKSampleHandler: RPBroadcastSampleHandler {
 
     override public func broadcastPaused() {
         // User has requested to pause the broadcast. Samples will stop being delivered.
+        DarwinNotificationCenter.shared.postNotification(.broadcastStopped)
+       clientConnection?.close()
     }
 
     override public func broadcastResumed() {
