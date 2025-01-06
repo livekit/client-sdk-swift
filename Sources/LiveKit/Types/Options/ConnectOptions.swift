9e952b2774f15d9f5b4b2b42881c518341ca49b7
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2025-01-06 20:34:54 +0900

    Fix ConnectOptions param
    ConnectOptions.socketConnectTimeoutInterval

--- Sources/LiveKit/Types/Options/ConnectOptions.swift
+++ Sources/LiveKit/Types/Options/ConnectOptions.swift
@@ -70,7 +70,7 @@ public final class ConnectOptions: NSObject, Sendable {
     public init(autoSubscribe: Bool = true,
                 reconnectAttempts: Int = 3,
                 reconnectAttemptDelay: TimeInterval = .defaultReconnectAttemptDelay,
-                connectTimeoutInterval: TimeInterval = .defaultSocketConnect,
+                socketConnectTimeoutInterval: TimeInterval = .defaultSocketConnect,
                 primaryTransportConnectTimeout: TimeInterval = .defaultTransportState,
                 publisherTransportConnectTimeout: TimeInterval = .defaultTransportState,
                 iceServers: [IceServer] = [],
@@ -80,7 +80,7 @@ public final class ConnectOptions: NSObject, Sendable {
         self.autoSubscribe = autoSubscribe
         self.reconnectAttempts = reconnectAttempts
         self.reconnectAttemptDelay = reconnectAttemptDelay
-        self.socketConnectTimeoutInterval = connectTimeoutInterval
+        self.socketConnectTimeoutInterval = socketConnectTimeoutInterval
         self.primaryTransportConnectTimeout = primaryTransportConnectTimeout
         self.publisherTransportConnectTimeout = publisherTransportConnectTimeout
         self.iceServers = iceServers
