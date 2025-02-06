50a9c4325eb94eeba58b40506e85be05153472b4
Jacob Gelman <3182119+ladvoc@users.noreply.github.com>
2025-02-06 13:11:52 -0800

    Refactor partition method

--- Sources/LiveKit/Broadcast/IPC/IPCChannel.swift
+++ Sources/LiveKit/Broadcast/IPC/IPCChannel.swift
@@ -124,7 +124,9 @@ final class IPCChannel: Sendable {
                 return try (decoder.decode(Header.self, from: data), nil)
             }
             let headerSize = data.count - payloadSize
-            let (headerData, payloadData) = data.partition(bytesInFirst: headerSize)
+            guard let (headerData, payloadData) = data.partition(bytesInFirst: headerSize) else {
+                throw Error.corruptMessage
+            }
             return try (decoder.decode(Header.self, from: headerData), payloadData)
         }
 
@@ -146,12 +148,12 @@ final class IPCChannel: Sendable {
 
 // MARK: - Extensions
 
-private extension Data {
-    func partition(bytesInFirst: Int) -> (Data, Data) {
-        guard bytesInFirst > 0 else { return (Data(), self) }
+extension Data {
+    func partition(bytesInFirst: Int) -> (Data, Data)? {
+        guard (0...count).contains(bytesInFirst) else { return nil }
         return (
-            subdata(in: 0 ..< Swift.min(bytesInFirst, count)),
-            subdata(in: Swift.min(bytesInFirst, count) ..< count)
+            subdata(in: 0 ..< bytesInFirst),
+            subdata(in: bytesInFirst ..< count)
         )
     }
 }
