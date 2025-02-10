cfe4247479f9f6c6768941b2c4e6281800d6e79d
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2025-02-11 00:40:43 +0900

    Fix test

--- Tests/LiveKitTests/MuteTests.swift
+++ Tests/LiveKitTests/MuteTests.swift
@@ -51,7 +51,7 @@ struct TestEngineStep {
     let assert: TestEngineAssert
 }
 
-extension RTCAudioEngineState: @retroactive CustomStringConvertible {
+extension RTCAudioEngineState: CustomStringConvertible {
     public var description: String {
         "EngineState(" +
             "outputEnabled: \(outputEnabled), " +
