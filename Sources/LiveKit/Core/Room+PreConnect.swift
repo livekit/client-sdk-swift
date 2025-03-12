6864075859377dc040058d9b586d13d9f1dd67be
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-03-12 14:58:37 +0100

    WIP: Start async

--- Sources/LiveKit/Core/Room+PreConnect.swift
+++ Sources/LiveKit/Core/Room+PreConnect.swift
@@ -18,6 +18,8 @@ import Foundation
 
 public extension Room {
     func startCapturingBeforeConnecting() {
-        preConnectBuffer.startRecording()
+        Task(priority: .userInitiated) {
+            await preConnectBuffer.startRecording()
+        }
     }
 }
