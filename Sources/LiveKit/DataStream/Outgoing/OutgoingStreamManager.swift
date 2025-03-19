26a7845f3b0409e2d7a295490b36f8fe9112c4e5
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-03-19 11:34:37 +0100

    Convert from/to ms

--- Sources/LiveKit/DataStream/Outgoing/OutgoingStreamManager.swift
+++ Sources/LiveKit/DataStream/Outgoing/OutgoingStreamManager.swift
@@ -272,9 +272,10 @@ extension Livekit_DataStream.Header {
         }
     }
 
+    // Stream timestamps are in ms (13 digits)
     var timestampDate: Date {
-        get { Date(timeIntervalSince1970: TimeInterval(timestamp)) }
-        set { timestamp = Int64(newValue.timeIntervalSince1970) }
+        get { Date(timeIntervalSince1970: TimeInterval(timestamp) / TimeInterval(MSEC_PER_SEC)) }
+        set { timestamp = Int64(newValue.timeIntervalSince1970 * TimeInterval(MSEC_PER_SEC)) }
     }
 }
 
