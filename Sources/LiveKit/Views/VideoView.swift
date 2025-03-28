f6579db212f94ec2824bd849e2a2b7a1062ccc7f
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-03-28 09:32:08 +0100

    Fix old Swift

--- Sources/LiveKit/Views/VideoView.swift
+++ Sources/LiveKit/Views/VideoView.swift
@@ -229,8 +229,14 @@ public class VideoView: NativeView, Loggable {
 
     // MARK: - Private
 
+    #if compiler(>=6.0)
     private nonisolated(unsafe) var _primaryRenderer: NativeRendererView?
     private nonisolated(unsafe) var _secondaryRenderer: NativeRendererView?
+    #else
+    private var _primaryRenderer: NativeRendererView?
+    private var _secondaryRenderer: NativeRendererView?
+    #endif
+
     private var _debugTextView: TextView?
 
     // used for stats timer
