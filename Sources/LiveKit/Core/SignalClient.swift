640aebaf0e50ef67f0e0b5287ba0b53e0893ee36
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2025-06-26 01:04:06 +0900

    Protocol 1.39.2 (#726)

--- Sources/LiveKit/Core/SignalClient.swift
+++ Sources/LiveKit/Core/SignalClient.swift
@@ -349,6 +349,9 @@ private extension SignalClient {
 
         case let .trackSubscribed(trackSubscribed):
             _delegate.notifyDetached { await $0.signalClient(self, didSubscribeTrack: Track.Sid(from: trackSubscribed.trackSid)) }
+
+        case .roomMoved:
+            log("Received roomMoved message")
         }
     }
 }
