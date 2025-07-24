81625ad8cff4ec30fd84480d16cad63eb7a57941
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-07-24 14:19:43 +0200

    Naming

--- Sources/LiveKit/Extensions/Codable.swift
+++ Sources/LiveKit/Extensions/Codable.swift
@@ -17,9 +17,9 @@
 import Foundation
 
 extension Encodable {
-    func transcoded<T: Decodable>(to type: T.Type,
-                                  encoder: JSONEncoder = JSONEncoder(),
-                                  decoder: JSONDecoder = JSONDecoder()) -> T?
+    func mapped<T: Decodable>(to type: T.Type,
+                              encoder: JSONEncoder = JSONEncoder(),
+                              decoder: JSONDecoder = JSONDecoder()) -> T?
     {
         guard let encoded = try? encoder.encode(self),
               let decoded = try? decoder.decode(type, from: encoded) else { return nil }
