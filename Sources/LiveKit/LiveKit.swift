d4c1370922f7f13df1d889ad408bb2298afa638b
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-08-29 12:46:01 +0200

    2.7.2 (#772)

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -33,7 +33,7 @@ let logger = Logger(label: "LiveKitSDK")
 @objc
 public class LiveKitSDK: NSObject {
     @objc(sdkVersion)
-    public static let version = "2.7.1"
+    public static let version = "2.7.2"
 
     @objc
     public static func setLoggerStandardOutput() {
