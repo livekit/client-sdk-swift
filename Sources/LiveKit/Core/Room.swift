748ddd654c544a874eda4dac36c51d1babd4f321
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-09-24 15:13:21 +0200

    Mutable variant with setRequest

--- Sources/LiveKit/Core/Room.swift
+++ Sources/LiveKit/Core/Room.swift
@@ -426,13 +426,12 @@ public class Room: NSObject, @unchecked Sendable, ObservableObject, Loggable {
     }
 
     public func connect(tokenSource: TokenSource,
-                        tokenOptions: Token.Options = .init(),
                         connectOptions: ConnectOptions? = nil,
                         roomOptions: RoomOptions? = nil) async throws
     {
         self.tokenSource = tokenSource
 
-        let token = try await tokenSource.fetch(tokenOptions)
+        let token = try await tokenSource.generate()
         try await connect(url: token.serverURL.absoluteString, token: token.participantToken, connectOptions: connectOptions, roomOptions: roomOptions)
     }
 
