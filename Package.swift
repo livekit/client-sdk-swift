f7590fd9560e6f054cffa6dc09116789f2f9bf75
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2025-10-01 22:36:08 +0800

    Format

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/livekit/webrtc-xcframework.git", exact: "137.7151.09"),
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.31.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.2"),
-        .package(url: "https://github.com/apple/swift-collections.git", "1.1.0"..<"1.3.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", "1.1.0" ..< "1.3.0"),
         // Only used for DocC generation
         .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0"),
         // Only used for Testing
