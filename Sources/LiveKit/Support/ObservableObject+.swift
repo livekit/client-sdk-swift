4fcd6510f7054e8894cef7a3e08acbceccf79290
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-10-16 13:53:56 +0200

    Sendable

--- Sources/LiveKit/Support/ObservableObject+.swift
+++ Sources/LiveKit/Support/ObservableObject+.swift
@@ -18,19 +18,13 @@
 
 extension ObservableObject {
     /// An async sequence that emits the `objectWillChange` events.
-    var changes: any AsyncSequence {
-        if #available(macOS 12.0, iOS 15.0, tvOS 15.0, *) {
-            // This is necessary due to ObservableObjectPublisher not respecting the demand.
-            // See: https://forums.swift.org/t/asyncpublisher-causes-crash-in-rather-simple-situation
-            objectWillChange.buffer(size: 1, prefetch: .byRequest, whenFull: .dropOldest).values
-        } else {
-            AsyncStream { continuation in
-                let cancellable = objectWillChange.sink { _ in
-                    continuation.yield()
-                }
-                continuation.onTermination = { _ in
-                    cancellable.cancel()
-                }
+    var changes: AsyncStream<Void> {
+        AsyncStream { continuation in
+            let cancellable = objectWillChange.sink { _ in
+                continuation.yield()
+            }
+            continuation.onTermination = { _ in
+                cancellable.cancel()
             }
         }
     }
