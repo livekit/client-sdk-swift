ddef539811647331775338ff4faf1b5309f56e8f
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2025-12-15 09:17:33 +0100

    v2.10.2 (#872)

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -32,7 +32,7 @@ public class LiveKitSDK: NSObject, Loggable {
     override private init() {}
 
     @objc(sdkVersion)
-    public static let version = "2.10.1"
+    public static let version = "2.10.2"
 
     fileprivate struct State {
         var logger: Logger = OSLogger()
