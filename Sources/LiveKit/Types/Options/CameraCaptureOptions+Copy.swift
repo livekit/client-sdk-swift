08a97c4bc04f759e5d31596c08c5a027be89793a
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2026-02-02 09:23:56 +0100

    feat: add deviceType to CameraCaptureOptions copyWith method

--- Sources/LiveKit/Types/Options/CameraCaptureOptions+Copy.swift
+++ Sources/LiveKit/Types/Options/CameraCaptureOptions+Copy.swift
@@ -17,16 +17,26 @@
 @preconcurrency import AVFoundation
 
 public extension CameraCaptureOptions {
-    func copyWith(device: ValueOrAbsent<AVCaptureDevice?> = .absent,
+    func copyWith(deviceType: ValueOrAbsent<AVCaptureDevice.DeviceType?> = .absent,
+                  device: ValueOrAbsent<AVCaptureDevice?> = .absent,
                   position: ValueOrAbsent<AVCaptureDevice.Position> = .absent,
                   preferredFormat: ValueOrAbsent<AVCaptureDevice.Format?> = .absent,
                   dimensions: ValueOrAbsent<Dimensions> = .absent,
                   fps: ValueOrAbsent<Int> = .absent) -> CameraCaptureOptions
     {
+        #if !os(visionOS)
+        CameraCaptureOptions(deviceType: deviceType.value(ifAbsent: self.deviceType),
+                             device: device.value(ifAbsent: self.device),
+                             position: position.value(ifAbsent: self.position),
+                             preferredFormat: preferredFormat.value(ifAbsent: self.preferredFormat),
+                             dimensions: dimensions.value(ifAbsent: self.dimensions),
+                             fps: fps.value(ifAbsent: self.fps))
+        #else
         CameraCaptureOptions(device: device.value(ifAbsent: self.device),
                              position: position.value(ifAbsent: self.position),
                              preferredFormat: preferredFormat.value(ifAbsent: self.preferredFormat),
                              dimensions: dimensions.value(ifAbsent: self.dimensions),
                              fps: fps.value(ifAbsent: self.fps))
+        #endif
     }
 }
