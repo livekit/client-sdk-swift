9859018394f1b34e3e04ae859debce96d855e1d8
Błażej Pankowski <86720177+pblazej@users.noreply.github.com>
2026-06-10 08:41:21 +0200

    v2.15.0 (#1027)
    - TF build 2.15.0.b20260603 🔴 (rejected)
    - TF build 2.15.0.b20260609

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -33,7 +33,7 @@ public class LiveKitSDK: NSObject, Loggable {
     override private init() {}
 
     @objc(sdkVersion)
-    public static let version = "2.14.1"
+    public static let version = "2.15.0"
     static let ffiVersion = buildVersion()
 
     fileprivate struct State {
