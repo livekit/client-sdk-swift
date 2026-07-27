43ade336353ad12f9ec7fa1e6a51d9b3bd8121f8
Hiroshi Horie <548776+hiroshihorie@users.noreply.github.com>
2026-07-28 03:29:39 +0900

    v2.15.3 (#1067)

--- Sources/LiveKit/LiveKit.swift
+++ Sources/LiveKit/LiveKit.swift
@@ -33,7 +33,7 @@ public class LiveKitSDK: NSObject, Loggable {
     override private init() {}
 
     @objc(sdkVersion)
-    public static let version = "2.15.2"
+    public static let version = "2.15.3"
     static let ffiVersion = buildVersion()
 
     fileprivate struct State {
